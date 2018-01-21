a807c8c29c5782f199d292291b04f333e193aace
Mustafa Kurugollu <2288113k@gmail.com>
2018-01-21 12:57:34 +0000

    Chp 3 finish
    Did all the tutorials and exercises

--- rango/urls.py
+++ rango/urls.py
@@ -1,12 +1,8 @@
 from django.conf.urls import url
-from django.contrib import admin
 from rango import views
 from django.conf.urls import include
 
-
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^rango/', include('rango.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'about/$', views.about, name='about'),
 ]
-
