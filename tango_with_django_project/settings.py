a6ae32678a7bd141c90f4cbc4be3cf7f6ef8c7c0
Mustafa Kurugollu <mkurugollu220@gmail.com>
2018-02-26 11:02:47 +0000

    chp 11 theory done (Not Ex)

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -33,6 +33,18 @@ DEBUG = True
 ALLOWED_HOSTS = []
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
+# If True, users can register
+REGISTRATION_OPEN = True
+# One-week activation window; you may, of course, use a different value.
+ACCOUNT_ACTIVATION_DAYS = 7
+# If True, the user will be automatically logged in.
+REGISTRATION_AUTO_LOGIN = True
+# The page you want users to arrive at after they successfully log in
+LOGIN_REDIRECT_URL = '/rango/'
+# The page users are directed to if they are not logged in,
+# and are trying to access pages requiring authentication
+LOGIN_URL = '/accounts/login/'
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -43,6 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -115,7 +128,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LOGIN_URL = '/rango/login/'
+LOGIN_URL = '/accounts/login/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
